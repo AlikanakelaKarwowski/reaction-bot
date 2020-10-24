6e96cd9741417a4727863d7b8373dcefdf1ef46a
A.Karwowski <akk42@zips.uakron.edu>
2020-10-24 15:19:48 -0400

    Initial commit

---
+++ reaction.py
@@ -0,0 +1,60 @@
+import discord
+import sys
+import time
+import logging
+from discord.utils import get
+time.localtime()
+intents = discord.Intents.all()
+logging.basicConfig(filename='log1.log', level=logging.INFO)
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    cur_time = time.strftime('%Y-%m-%d %A %H:%M:%S',time.localtime())
+    logging.info(f"{cur_time} : Bot is logged in.")
+
+@client.event 
+async def on_raw_reaction_add(payload):
+    message_id = payload.message_id
+    if message_id == 769610274256715837:
+        guild_id = payload.guild_id
+        guild = discord.utils.find(lambda g : g.id == guild_id, client.guilds)
+
+        role = discord.utils.get(guild.roles, name=payload.emoji.name)
+
+        if role is not None:
+            logging.info(role.name)
+            member = payload.member
+            if member is not None:
+                await member.add_roles(role)
+            else:
+                cur_time = time.strftime('%Y-%m-%d %A %H:%M:%S', time.localtime())
+                logging.info(f"{cur_time} : Member not found")
+        else:
+            cur_time = time.strftime('%Y-%m-%d %A %H:%M:%S', time.localtime())
+            logging.info(f"{cur_time} : {role} Role not found")
+@client.event
+async def on_raw_reaction_remove(payload):
+    message_id = payload.message_id
+    if message_id == 769610274256715837:
+        guild_id = payload.guild_id
+        guild = discord.utils.find(lambda g: g.id == guild_id, client.guilds)
+
+        role = discord.utils.get(guild.roles, name=payload.emoji.name)
+
+        if role is not None:
+            logging.info(role.name)
+            member = get(guild.members, id=payload.user_id)
+            #member = discord.utils.get(guild.members, name=payload.user_id)
+            print(member)
+            if member is not None:
+                await member.remove_roles(role)
+            else:
+                cur_time = time.strftime('%Y-%m-%d %A %H:%M:%S', time.localtime())
+                logging.info(f"{cur_time} : Member not found")
+        else:
+            cur_time = time.strftime('%Y-%m-%d %A %H:%M:%S', time.localtime())
+            logging.info(f"{cur_time} : {role} Role not found")
+
+
+client.run('NzQ3NTEwOTQ3NTU5NzY4MTk0.X0P70Q.H6I1jBBKis2CGjaVD4NRhgHEph0')
