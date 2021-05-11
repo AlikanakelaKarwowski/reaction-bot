e1954fd09c35aad469b172b1d03455d2adab5c95
Alikanakela <alexkarrrr@gmail.com>
2021-05-11 18:07:31 +0000

    Add extra message and fix it for the new server id

--- reaction.py
+++ reaction.py
@@ -1,64 +1,58 @@
+#!/usr/bin/python3
+from clientkey import key
 import discord
 import sys
 import time
 import logging
 from discord.utils import get
+
 time.localtime()
 intents = discord.Intents.all()
-logging.basicConfig(filename='log1.log', level=logging.INFO)
+logging.basicConfig(filename='/reaction-bot/log1.log', level=logging.INFO)
 client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
     cur_time = time.strftime('%Y-%m-%d %A %H:%M:%S',time.localtime())
-    logging.info(f"{cur_time} : Bot is logged in.")
+    logging.info(f"{cur_time} | Bot sucessfully logged in.")
 
-@client.event 
+@client.event
 async def on_raw_reaction_add(payload):
     message_id = payload.message_id
-    if message_id == 769645416672919572:
+    if message_id == 816697911781621761 or message_id == 816703783077019649:
         guild_id = payload.guild_id
         guild = discord.utils.find(lambda g : g.id == guild_id, client.guilds)
+#        student = discord.utils.get(guild.roles, "Student")
+        role = discord.utils.get(guild.roles, name=payload.emoji.name)
 
-        if payload.emoji.name == 'OTP':
-            role = discord.utils.get(guild.roles, name='Off Topic Ping')
-        else:
-            role = discord.utils.get(guild.roles, name=payload.emoji.name)
-        
         if role is not None:
-            logging.info(role.name)
+            cur_time = time.strftime('%Y-%m-%d %A %H:%M:%S',time.localtime())
             member = payload.member
             if member is not None:
+                logging.info(f"{cur_time} | {member}: was assigned {role.name}")
                 await member.add_roles(role)
-            else:
-                cur_time = time.strftime('%Y-%m-%d %A %H:%M:%S', time.localtime())
-                logging.info(f"{cur_time} : Member not found")
-        else:
-            cur_time = time.strftime('%Y-%m-%d %A %H:%M:%S', time.localtime())
-            logging.info(f"{cur_time} : {role} Role not found")
+ #               await member.add_roles(student)
 @client.event
 async def on_raw_reaction_remove(payload):
     message_id = payload.message_id
-    if message_id == 769645416672919572:
+    if message_id == 816697911781621761 or message_id == 816703783077019649:
         guild_id = payload.guild_id
         guild = discord.utils.find(lambda g: g.id == guild_id, client.guilds)
 
-        if payload.emoji.name == 'OTP':
-            role = discord.utils.get(guild.roles, name='Off Topic Ping')
-        else:
-            role = discord.utils.get(guild.roles, name=payload.emoji.name)
+        role = discord.utils.get(guild.roles, name=payload.emoji.name)
 
         if role is not None:
-            logging.info(role.name)
+            cur_time = time.strftime('%Y-%m-%d %A %H:%M:%S',time.localtime())
             member = get(guild.members, id=payload.user_id)
             if member is not None:
+                logging.info(f"{cur_time} | {member}: was unassigned {role.name}")
                 await member.remove_roles(role)
             else:
                 cur_time = time.strftime('%Y-%m-%d %A %H:%M:%S', time.localtime())
-                logging.info(f"{cur_time} : Member not found")
+                logging.info(f"{cur_time} | Member not found")
         else:
             cur_time = time.strftime('%Y-%m-%d %A %H:%M:%S', time.localtime())
-            logging.info(f"{cur_time} : {role} Role not found")
+            logging.info(f"{cur_time} | {role.name} Role not found")
 
 
-client.run('NzQ3NTEwOTQ3NTU5NzY4MTk0.X0P70Q.H6I1jBBKis2CGjaVD4NRhgHEph0')
+client.run(key)
